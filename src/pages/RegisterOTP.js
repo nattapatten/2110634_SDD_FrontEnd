7a2123cd2d8c4e35a98545e6f8afcdd334c9e661
Your Name <you@example.com>
2024-04-04 22:11:54 +0700

    Siripat_TeemUpdate4Apr24

--- src/pages/RegisterOTP.js
+++ src/pages/RegisterOTP.js
@@ -1,5 +1,34 @@
-const RegisterOTP = () => {
-    return <h1>RegisterOTP</h1>;
-  };
-  
-  export default RegisterOTP;
+import React from 'react';
+import './RegisterOTP.css';
+import chulaLogo from '../assets/chula_logo_Login.png'; 
+import OTP from '../assets/OTP.png'; // Make sure the path and file name are correct
+
+function RegisterOTP() {
+  return (
+    <div className="otp-wrapper">
+      <div className="otp-logo">
+        <img src={chulaLogo} alt="Chulalongkorn University Logo" className="chula-logo" />
+      </div>
+      <div className="otp-container">
+      <img src={OTP} alt="OTP Icon" className="otp-icon" /> 
+        <h2>Please check your email</h2>
+        <p>We’ve sent a code to 6Xxxxxxxx@student.chula.ac.th</p>
+        <form>
+          <div className="otp-form-group">
+            {[...Array(4)].map((_, index) => (
+              <input key={index} type="text" maxLength="1" required />
+            ))}
+          </div>
+          <h5>Didn't get the code?</h5>
+          <button type="button" className="otp-resend">Click to resend</button>
+          <div className="otp-actions">
+            <button type="button" className="otp-cancel2">Cancel</button>
+            <button type="submit" className="btn-primary2">Verify</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default RegisterOTP;
